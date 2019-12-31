90e948a59a4681550cc9f5251fa11bfa3943fee8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:14:00 -0600

    Rewriting as Laminas Project package

--- test/Adapter/DbTable/CallbackCheckAdapterTest.php
+++ test/Adapter/DbTable/CallbackCheckAdapterTest.php
@@ -1,36 +1,35 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-authentication for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-authentication/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-authentication/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Authentication\Adapter\DbTable;
+namespace LaminasTest\Authentication\Adapter\DbTable;
 
-use Zend\Authentication;
-use Zend\Authentication\Adapter;
-use Zend\Db\Adapter\Adapter as DbAdapter;
-use Zend\Db\Sql\Select as DBSelect;
+use Laminas\Authentication;
+use Laminas\Authentication\Adapter;
+use Laminas\Db\Adapter\Adapter as DbAdapter;
+use Laminas\Db\Sql\Select as DBSelect;
 
 /**
- * @group      Zend_Auth
- * @group      Zend_Db_Table
+ * @group      Laminas_Auth
+ * @group      Laminas_Db_Table
  */
 class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * SQLite database connection
      *
-     * @var \Zend\Db\Adapter\Adapter
+     * @var \Laminas\Db\Adapter\Adapter
      */
     protected $_db = null;
 
     /**
      * Database table authentication adapter
      *
-     * @var \Zend\Authentication\Adapter\DbTable
+     * @var \Laminas\Authentication\Adapter\DbTable
      */
     protected $_adapter = null;
 
@@ -39,8 +38,8 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function setUp()
     {
-        if (!defined('TESTS_ZEND_AUTH_ADAPTER_DBTABLE_PDO_SQLITE_ENABLED') ||
-            constant('TESTS_ZEND_AUTH_ADAPTER_DBTABLE_PDO_SQLITE_ENABLED') === false
+        if (!defined('TESTS_LAMINAS_AUTH_ADAPTER_DBTABLE_PDO_SQLITE_ENABLED') ||
+            constant('TESTS_LAMINAS_AUTH_ADAPTER_DBTABLE_PDO_SQLITE_ENABLED') === false
         ) {
             $this->markTestSkipped('Tests are not enabled in TestConfiguration.php');
             return;
@@ -96,7 +95,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
     public function testAuthenticateCallbackThrowsException()
     {
         $this->setExpectedException(
-            'Zend\Authentication\Adapter\Dbtable\Exception\InvalidArgumentException',
+            'Laminas\Authentication\Adapter\Dbtable\Exception\InvalidArgumentException',
             'Invalid callback provided'
         );
         $this->_adapter->setCredentialValidationCallback('This is not a valid callback');
@@ -181,7 +180,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @group ZF-5957
+     * @group Laminas-5957
      */
     public function testAdapterCanReturnDbSelectObject()
     {
@@ -189,7 +188,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @group ZF-5957
+     * @group Laminas-5957
      */
     public function testAdapterCanUseModifiedDbSelectObject()
     {
@@ -203,7 +202,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @group ZF-5957
+     * @group Laminas-5957
      */
     public function testAdapterReturnsASelectObjectWithoutAuthTimeModificationsAfterAuth()
     {
@@ -226,7 +225,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testCatchExceptionNoTable()
     {
-        $this->setExpectedException('Zend\Authentication\Adapter\Dbtable\Exception\RuntimeException',
+        $this->setExpectedException('Laminas\Authentication\Adapter\Dbtable\Exception\RuntimeException',
                                     'A table must be supplied for');
         $adapter = new Adapter\DbTable($this->_db);
         $adapter->authenticate();
@@ -237,7 +236,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testCatchExceptionNoIdentityColumn()
     {
-        $this->setExpectedException('Zend\Authentication\Adapter\Dbtable\Exception\RuntimeException',
+        $this->setExpectedException('Laminas\Authentication\Adapter\Dbtable\Exception\RuntimeException',
                                     'An identity column must be supplied for the');
         $adapter = new Adapter\DbTable($this->_db, 'users');
         $adapter->authenticate();
@@ -248,7 +247,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testCatchExceptionNoCredentialColumn()
     {
-        $this->setExpectedException('Zend\Authentication\Adapter\Dbtable\Exception\RuntimeException',
+        $this->setExpectedException('Laminas\Authentication\Adapter\Dbtable\Exception\RuntimeException',
                                     'A credential column must be supplied');
         $adapter = new Adapter\DbTable($this->_db, 'users', 'username');
         $adapter->authenticate();
@@ -259,7 +258,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testCatchExceptionNoIdentity()
     {
-        $this->setExpectedException('Zend\Authentication\Adapter\Dbtable\Exception\RuntimeException',
+        $this->setExpectedException('Laminas\Authentication\Adapter\Dbtable\Exception\RuntimeException',
                                     'A value for the identity was not provided prior');
         $this->_adapter->authenticate();
     }
@@ -269,7 +268,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testCatchExceptionNoCredential()
     {
-        $this->setExpectedException('Zend\Authentication\Adapter\Dbtable\Exception\RuntimeException',
+        $this->setExpectedException('Laminas\Authentication\Adapter\Dbtable\Exception\RuntimeException',
                                     'A credential value was not provided prior');
         $this->_adapter->setIdentity('my_username');
         $this->_adapter->authenticate();
@@ -280,7 +279,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testCatchExceptionBadSql()
     {
-        $this->setExpectedException('Zend\Authentication\Adapter\Dbtable\Exception\RuntimeException',
+        $this->setExpectedException('Laminas\Authentication\Adapter\Dbtable\Exception\RuntimeException',
                                     'The supplied parameters to');
         $this->_adapter->setTableName('bad_table_name');
         $this->_adapter->setIdentity('value');
@@ -291,9 +290,9 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
     /**
      * Test to see same usernames with different passwords can not authenticate
      * when flag is not set. This is the current state of
-     * Zend_Auth_Adapter_DbTable (up to ZF 1.10.6)
+     * Laminas_Auth_Adapter_DbTable (up to Laminas 1.10.6)
      *
-     * @group   ZF-7289
+     * @group   Laminas-7289
      */
     public function testEqualUsernamesDifferentPasswordShouldNotAuthenticateWhenFlagIsNotSet()
     {
@@ -314,7 +313,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
      * Test to see same usernames with different passwords can authenticate when
      * a flag is set
      *
-     * @group   ZF-7289
+     * @group   Laminas-7289
      */
     public function testEqualUsernamesDifferentPasswordShouldAuthenticateWhenFlagIsSet()
     {
@@ -350,7 +349,7 @@ class CallbackCheckAdapterTest extends \PHPUnit_Framework_TestCase
     protected function _setupDbAdapter($optionalParams = array())
     {
         $params = array('driver' => 'pdo_sqlite',
-                        'dbname' => TESTS_ZEND_AUTH_ADAPTER_DBTABLE_PDO_SQLITE_DATABASE);
+                        'dbname' => TESTS_LAMINAS_AUTH_ADAPTER_DBTABLE_PDO_SQLITE_DATABASE);
 
         if (!empty($optionalParams)) {
             $params['options'] = $optionalParams;
