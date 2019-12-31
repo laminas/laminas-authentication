b5dddee6a5e7895472a8f1a3e2fdc5a6514f5057
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:14:06 -0600

    Rewriting as Laminas Project package

--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-authentication for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-authentication/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-authentication/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Authentication\Storage;
+namespace Laminas\Authentication\Storage;
 
 interface StorageInterface
 {
     /**
      * Returns true if and only if storage is empty
      *
-     * @throws \Zend\Authentication\Exception\ExceptionInterface If it is impossible to determine whether storage is empty
+     * @throws \Laminas\Authentication\Exception\ExceptionInterface If it is impossible to determine whether storage is empty
      * @return bool
      */
     public function isEmpty();
@@ -24,7 +23,7 @@ interface StorageInterface
      *
      * Behavior is undefined when storage is empty.
      *
-     * @throws \Zend\Authentication\Exception\ExceptionInterface If reading contents from storage is impossible
+     * @throws \Laminas\Authentication\Exception\ExceptionInterface If reading contents from storage is impossible
      * @return mixed
      */
     public function read();
@@ -33,7 +32,7 @@ interface StorageInterface
      * Writes $contents to storage
      *
      * @param  mixed $contents
-     * @throws \Zend\Authentication\Exception\ExceptionInterface If writing $contents to storage is impossible
+     * @throws \Laminas\Authentication\Exception\ExceptionInterface If writing $contents to storage is impossible
      * @return void
      */
     public function write($contents);
@@ -41,7 +40,7 @@ interface StorageInterface
     /**
      * Clears contents from storage
      *
-     * @throws \Zend\Authentication\Exception\ExceptionInterface If clearing contents from storage is impossible
+     * @throws \Laminas\Authentication\Exception\ExceptionInterface If clearing contents from storage is impossible
      * @return void
      */
     public function clear();
