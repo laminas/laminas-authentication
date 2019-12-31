67fb856108ca9c4c7fa6c52c44eed26199eb55e8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:13:27 -0600

    Rewriting as Laminas Project package

--- src/Adapter/Exception/ExceptionInterface.php
+++ src/Adapter/Exception/ExceptionInterface.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Authentication
+ * @see       https://github.com/laminas/laminas-authentication for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-authentication/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-authentication/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Authentication\Adapter\Exception;
+namespace Laminas\Authentication\Adapter\Exception;
 
-use Zend\Authentication\Exception\ExceptionInterface as Exception;
+use Laminas\Authentication\Exception\ExceptionInterface as Exception;
 
 /**
- * @category   Zend
- * @package    Zend_Authentication
+ * @category   Laminas
+ * @package    Laminas_Authentication
  * @subpackage Adapter_Exception
  */
 interface ExceptionInterface extends Exception
