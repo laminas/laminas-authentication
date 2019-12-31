7f9bdc4caeba12cede122fed97e59b64a0119544
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:13:39 -0600

    Rewriting as Laminas Project package

--- src/Adapter/Digest.php
+++ src/Adapter/Digest.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-authentication for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-authentication/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-authentication/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Authentication\Adapter;
+namespace Laminas\Authentication\Adapter;
 
-use Zend\Authentication\Result as AuthenticationResult;
-use Zend\Stdlib\ErrorHandler;
+use Laminas\Authentication\Result as AuthenticationResult;
+use Laminas\Stdlib\ErrorHandler;
 
 class Digest extends AbstractAdapter
 {
@@ -139,7 +138,7 @@ class Digest extends AbstractAdapter
     }
 
     /**
-     * Defined by Zend\Authentication\Adapter\AdapterInterface
+     * Defined by Laminas\Authentication\Adapter\AdapterInterface
      *
      * @throws Exception\ExceptionInterface
      * @return AuthenticationResult
