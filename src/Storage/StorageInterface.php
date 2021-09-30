d56b32a75d08d4bd258676a0c9f59f71751a0c5a
Andi Rückauer <arueckauer@gmail.com>
2021-09-30 09:07:20 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-authentication for the canonical source repository
- * @copyright https://github.com/laminas/laminas-authentication/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-authentication/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\Authentication\Storage;
 
 interface StorageInterface
